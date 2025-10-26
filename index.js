05a74d91c5fc6a02818d9a1f60bf44fcfc97d80f
Sebasbanto <sbanderastorres@gmail.com>
2025-10-26 18:58:03 -0500

    x

--- index.js
+++ index.js
@@ -2,8 +2,3 @@ import app from "./app.js";
 import { connectDB } from "./bd.js";
 
 connectDB();
-if (process.env.NODE_ENV !== "production") {
-  app.listen(3000, () =>
-    console.log("Server running on http://localhost:3000")
-  );
-}
